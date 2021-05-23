9114dfbed19b7330f77ebc07ddc0e98127b32bab
wiolettamehiadin <wioletta_k@live.co.uk>
2021-05-23 22:16:49 +0100

    Rozpoczecie pliku

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let str1 = 'Uwielbiam JavaScript';
+let str2 = 'Jestem świetnym programistą';
+
+function findLongestSentence (str1, str2) {
+ if (str1 < str2);
+ return str2;
+
+}
+
+console.log (findLongestSentence (str1, str2));
