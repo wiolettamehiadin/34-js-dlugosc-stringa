fd6d7c0c2722e7c272a4271b5664b28df11f468f
wiolettamehiadin <wioletta_k@live.co.uk>
2021-05-25 22:09:57 +0100

    Poprawka

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-let str1 = 'Uwielbiam JavaScript';
-let str2 = 'Jestem świetnym programistą';
+let sentence1 = 'Uwielbiam JavaScript';
+let sentence2 = 'Jestem świetnym programistą';
+
+
+
 
 function findLongestSentence (str1, str2) {
- if (str1 < str2);
- return str2;
-
-}
-
-console.log (findLongestSentence (str1, str2));
+    if (str1.length < str2.length) {
+       return sentence2; 
+  } else (str1.length > str2.length);
+       return sentence1;
+  } 
+   console.log (findLongestSentence (sentence1, sentence2));
